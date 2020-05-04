359e87c789fc82e534b11919492c1527c67217d9
harmanb <hsbajwa@ryerson.ca>
2020-05-04 13:34:22 -0400

    fixed dependency issue

--- src/components/Search.js
+++ src/components/Search.js
@@ -2,7 +2,7 @@
 import React, {Component} from 'react';
 import Navbar from 'react-bootstrap/Navbar';
 import Form from 'react-bootstrap/Form';
-import FormControl from 'react-bootstrap/Formcontrol';
+import FormControl from 'react-bootstrap/FormControl';
 import Button from 'react-bootstrap/Button';
 import { connect } from 'react-redux';
 import { restaurantSearch } from '../actions';
